5306306be9b26ce2b0d5af5dfeeccbea10978660
Helio Andrade <hmajr.tec@gmail.com>
2024-07-20 18:32:03 -0300

    Create app.tsx, Add Header component

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+import { Header } from './components/Header'
+
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-5 flex flex-col gap-16'>
+        <Header />
+
+      </div>
+    </div>   
+  )
+}
