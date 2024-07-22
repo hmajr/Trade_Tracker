54f45e1ef8d2845da1814bece53035140db4cbca
Helio Andrade <hmajr.tec@gmail.com>
2024-07-22 01:10:08 -0300

    Add summary days table

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { View, Text , ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Header } from "../components/Header";
-import { HabitDay , DAY_SIZE} from "../components/HabitDay";
+import { TradeDay , DAY_SIZE} from "../components/TradeDay";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
 const weekDays = ['D','S','T','Q','Q','S','S'];
@@ -18,6 +18,47 @@ export function Home(){
       
       <Header />
       
+      <View className="flex-row mt-6 mb-2 justify-between">
+        {
+          weekDays.map((weekDay, i) =>(
+            <Text 
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{width: DAY_SIZE}}
+            >
+              {weekDay}
+            </Text>
+          ))
+        }
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 100}}
+      >
+        <View className="flex-row flex-wrap">
+          {
+            datesFromYearStart.map(date => (
+              <TradeDay
+                key={date.toISOString()}
+                onPress={() => navigate ('habit', { date: date.toISOString() })}
+              />
+            ))
+          }
+
+          {
+            ammountOfDaysToFill > 0 && Array
+              .from({ length: ammountOfDaysToFill})
+            .map((_, index) =>(
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))
+          }
+        </View > 
+      </ScrollView>
     </View>
   )
 }
