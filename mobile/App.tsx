6385247ea77c8ac24a6cd2d92ae1491ffc506cdd
Helio Andrade <hmajr.tec@gmail.com>
2024-07-22 00:41:33 -0300

    Add app fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,29 @@
 
 import './src/lib/dayjs';
 import { StatusBar } from 'react-native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
 
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <>
