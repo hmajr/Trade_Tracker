c3a81847f0974d60e1557dfd6e6d165ab3f86c95
Helio Andrade <hmajr.tec@gmail.com>
2024-07-22 00:37:33 -0300

    Create App entry file

---
+++ mobile/App.tsx
@@ -0,0 +1,18 @@
+
+import './src/lib/dayjs';
+import { StatusBar } from 'react-native';
+
+
+export default function App() {
+
+  return (
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+    </>
+  );
+}
+
+// docker + kubernets (containers)
+// jakins
+// aws
+// ci-cd -> git actions (pr)
