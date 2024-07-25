724aeb53d03ff6322d66fc233fbab907f55d36a5
Helio Andrade <hmajr.tec@gmail.com>
2024-07-25 03:25:40 -0300

    Create interface to receave server data

--- mobile/src/components/TradeDay.tsx
+++ mobile/src/components/TradeDay.tsx
@@ -6,9 +6,13 @@ const SCREEN_HORIZONTAL_PADDING = (32*2)/5;
 export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5)
 
-interface Props extends TouchableOpacityProps { }
+interface Props extends TouchableOpacityProps { 
+  amountOfTrades?: number
+  amountOfWinnerTrades?: number
+  date: Date
+}
 
-export function TradeDay ({...rest} : Props){
+export function TradeDay ({amountOfTrades = 0, amountOfWinnerTrades = 0, date, ...rest} : Props){
   return (
     <TouchableOpacity 
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
