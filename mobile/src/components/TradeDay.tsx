7951ff67c96cc9414aef07c8e411aca115354cc7
Helio Andrade <hmajr.tec@gmail.com>
2024-07-26 16:33:25 -0300

    Add custom tradesDay style

--- mobile/src/components/TradeDay.tsx
+++ mobile/src/components/TradeDay.tsx
@@ -1,4 +1,7 @@
 import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
+import clsx from "clsx";
+import dayjs from "dayjs";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32*2)/5;
@@ -13,10 +16,25 @@ interface Props extends TouchableOpacityProps {
 }
 
 export function TradeDay ({amountOfTrades = 0, amountOfWinnerTrades = 0, date, ...rest} : Props){
+  
+  const winnerPercentage = amountOfTrades > 0 ? generateProgressPercentage(amountOfTrades, amountOfWinnerTrades) : 0
+  const today = dayjs().startOf('day').toDate()
+  const isCurrentDay = dayjs(date).isSame(today)
+
   return (
     <TouchableOpacity 
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      className={clsx('rounded-lg border-2 m-1', {
+        'bg-yellow-400 border-orange-300' : winnerPercentage == 100,
+        'bg-green-500 border-green-400' : winnerPercentage >= 80 && winnerPercentage < 100,
+        'bg-green-600 border-green-500' : winnerPercentage >= 60 && winnerPercentage < 80,
+        'bg-green-700 border-green-600' : winnerPercentage >= 40 && winnerPercentage < 60,
+        'bg-green-800 border-green-700' : winnerPercentage >= 20 && winnerPercentage < 40 ,
+        'bg-green-900 border-green-800' : winnerPercentage > 0 && winnerPercentage < 20,
+        'bg-zinc-900 border-zinc-800' : winnerPercentage == 0,
+        'border-white border-4' : isCurrentDay
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE}}
+      activeOpacity={0.7}
       {...rest}
     />
   )
