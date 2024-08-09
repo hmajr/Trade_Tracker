b240bc9d59d636d6da3dfbc2853e771f65657e18
Helio Andrade <hmajr.tec@gmail.com>
2024-08-09 18:56:25 -0300

    Add timezone, utc support future timezone handle

--- web/src/lib/dayjs.ts
+++ web/src/lib/dayjs.ts
@@ -1,4 +1,8 @@
 import dayjs from "dayjs";
 import 'dayjs/locale/pt-br'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
 
+dayjs.extend(timezone)
+dayjs.extend(utc)
 dayjs.locale('pt-br')
