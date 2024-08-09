496e79f37b2530a8c8d671d4884cc18a3861853d
Helio Andrade <hmajr.tec@gmail.com>
2024-08-09 14:49:46 -0300

    Fix Novo Trade name button

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -28,7 +28,7 @@ export function Header(){
             </Dialog.Close>
 
             <Dialog.Title className='text-3xl leading-tight font-extrabold'>
-              Criar Hábito
+              Criar Trade
             </Dialog.Title>
 
             <NewTradeForm />
