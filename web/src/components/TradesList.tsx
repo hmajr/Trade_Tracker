0dfdbe79ed8705441aace1ab8801e6d4c0f3ebab
Helio Andrade <hmajr.tec@gmail.com>
2024-08-10 01:19:50 -0300

    Include interface to recognize server response

--- web/src/components/TradesList.tsx
+++ web/src/components/TradesList.tsx
@@ -15,30 +15,34 @@ interface TradesInfo {
   exit_date: Date
 }
 
+interface DayTradesResponse {
+  possibleTrades: TradesInfo[];
+  winTrades: string[]; 
+}
+
 export function TradesList( {date}: TradesListProps) {
-  const [tradesInfo, setTradesInfo] = useState()
+  const [tradesInfo, setTradesInfo] = useState<DayTradesResponse | null>(null)
 
   useEffect(() => {
-    api.get('dayTrades', {
+    api.get<DayTradesResponse>('dayTrades', { // Specify the expected response type
       params: {
         date: date.toISOString(),
       }
     }).then(response => {
       setTradesInfo(response.data)
     })
-    
   }, [])
   
   return (
     <div className='mt-6 flex flex-col gap-3'>
       {/* Popover Trade Details */}
-      {tradesInfo?.possibleTrades.map(trade =>{
+      {tradesInfo?.possibleTrades.map(trade =>{ 
         return(
           <Popover.Root key={`${trade.id}-pop`}>
             <Popover.Trigger>
-                <span className='font-semibold text-xl text-white leading-tight'>
+              <span className='font-semibold text-xl text-white leading-tight'>
                   {trade.ticker} | {trade.result >= 0 ? `R$ ${trade.result}` : `-R$ ${trade.result*(-1)}`}
-                </span>
+              </span>
             </Popover.Trigger>
             <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 border-2 border-zinc-500 flex flex-col'>
               <TradeDetail
