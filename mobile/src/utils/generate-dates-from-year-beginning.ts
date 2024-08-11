77b07f97d7e79bbf9c990f0640999c527ede2034
Helio Andrade <hmajr.tec@gmail.com>
2024-08-10 21:40:15 -0300

    Update start date

--- mobile/src/utils/generate-dates-from-year-beginning.ts
+++ mobile/src/utils/generate-dates-from-year-beginning.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 export function generateDatesFromYearBeginning(){
 
   // const firstDayOfTheYear = dayjs().startOf('year')
-  const firstDayOfTheYear = dayjs().startOf('month')
+  const firstDayOfTheYear = dayjs().startOf('year')
   const today = new Date()
 
   const dates = []
