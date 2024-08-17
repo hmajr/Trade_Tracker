efe0300cf2d8c146cd2a389c70b012e5170df3cb
Helio Andrade <hmajr.tec@gmail.com>
2024-08-16 21:29:24 -0300

    Build it up component with all dialog ui

--- web/src/components/EditTradeForm.tsx
+++ web/src/components/EditTradeForm.tsx
@@ -1,8 +1,11 @@
 import { Check } from "phosphor-react";
+import { X } from 'phosphor-react'
 import { FormEvent, useState } from "react";
 import { api } from "../lib/axios";
 import dayjs from "dayjs";
 import { FORMAT_STYLE } from "../lib/dayjs";
+import * as Dialog from '@radix-ui/react-dialog'
+
 
 interface editTradeProps{
   id : string,
@@ -10,7 +13,7 @@ interface editTradeProps{
   result : number,
   entryDate : Date,
   exitDate : Date 
-  onTradeEdited: () => void
+  onShowTradeEdited: () => void
 }
 
 export function EditTradeForm(props : editTradeProps){
@@ -22,10 +25,9 @@ export function EditTradeForm(props : editTradeProps){
     
     if( !ticker.trim() )
     {
-      return (console.log("TRADE INVALIDO!!!"))
+      return (alert("TICKER INVALIDO!!!"))
     }
     
-    console.log(`${ticker} and ${result}`)
     await api.patch(`trade/${props.id}/edit`, {
       ticker,
       result
@@ -33,7 +35,7 @@ export function EditTradeForm(props : editTradeProps){
         console.log('Trade updated successfully:', response.data);
         alert('Trade editado!')
         
-        props.onTradeEdited()
+        props.onShowTradeEdited()
       })
       .catch((error) => {
         console.error('Error updating trade:', error);
@@ -41,65 +43,76 @@ export function EditTradeForm(props : editTradeProps){
   }
 
   return (
-    <form onSubmit={handleEditTrade} className="w-full flex flex-col mt-6">
-      <label htmlFor="ticker">
-        Qual o ticker?
-      </label>
+    <Dialog.Portal>
+      <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0'/>
+      <Dialog.Content className='absolute p-10 bg bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <Dialog.Title className='text-3xl leading-tight font-extrabold'>
+          Editar Trade
+        </Dialog.Title>
+        <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200 hover:border-red-500'>
+          <X size={24} aria-label='Fechar'/>
+        </Dialog.Close>
+        <form onSubmit={handleEditTrade} className="w-full flex flex-col mt-6">
+          <label htmlFor="ticker">
+            Qual o ticker?
+          </label>
 
-      <input 
-        type="text"
-        id="ticker"
-        placeholder={props.ticker}
-        className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
-        autoFocus
-        onChange={event => setTicker(event.target.value)}
-        />
+          <input 
+            type="text"
+            id="ticker"
+            placeholder={props.ticker}
+            className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+            autoFocus
+            onChange={event => setTicker(event.target.value)}
+            />
 
-      <label htmlFor="" className="font-semibold flex leading-tight mt-4">
-        Qual o Resultado?
-      </label>
+          <label htmlFor="" className="font-semibold flex leading-tight mt-4">
+            Qual o Resultado?
+          </label>
 
-      <input 
-        type="number"
-        id="result"
-        placeholder={props.result.toString()}
-        className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
-        autoFocus
-        onChange={event => setResult( Number(event.target.value) )}
-        />
+          <input 
+            type="number"
+            id="result"
+            placeholder={props.result.toString()}
+            className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+            autoFocus
+            onChange={event => setResult( Number(event.target.value) )}
+            />
 
-      
-      <label className="font-semibold flex leading-tight mt-4">
-        Data/Hora Entrada <span className="font-normal">&nbsp;(somente leitura)</span>
-      </label>
+          
+          <label className="font-semibold flex leading-tight mt-4">
+            Data/Hora Entrada <span className="font-normal">&nbsp;(somente leitura)</span>
+          </label>
 
-      <input readOnly
-        type="text"
-        id="entry"
-        value={dayjs(props.entryDate).format(FORMAT_STYLE)}
-        className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
-      />
+          <input readOnly
+            type="text"
+            id="entry"
+            value={dayjs(props.entryDate).format(FORMAT_STYLE)}
+            className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+          />
 
-      
-      <label className="font-semibold flex leading-tight mt-4">
-        Data/Hora Saída <span className="font-normal">&nbsp;(somente leitura)</span>
-      </label>
+          
+          <label className="font-semibold flex leading-tight mt-4">
+            Data/Hora Saída <span className="font-normal">&nbsp;(somente leitura)</span>
+          </label>
 
-      <input readOnly
-        type="text"
-        id="exit"
-        value={dayjs(props.exitDate).format(FORMAT_STYLE)}
-        className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
-      />
-      
-      <button 
-        type="submit" 
-        className='mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold transition-colors bg-green-600 hover:bg-green-400'
-        onClick={handleEditTrade}
-      >
-        <Check size={20} weight="bold" />  
-        Confirmar
-      </button>
-    </form>
+          <input readOnly
+            type="text"
+            id="exit"
+            value={dayjs(props.exitDate).format(FORMAT_STYLE)}
+            className="p-4 block rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+          />
+          
+          <button 
+            type="submit" 
+            className='mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold transition-colors bg-green-600 hover:bg-green-400'
+            onClick={handleEditTrade}
+          >
+            <Check size={20} weight="bold" />  
+            Confirmar
+          </button>
+        </form>
+      </Dialog.Content>
+    </Dialog.Portal>
   )
 }
