6e607173830500723f2f7b6dc9518050f5af58fc
Helio Andrade <hmajr.tec@gmail.com>
2024-08-16 21:34:05 -0300

    Update the delete and edit trade components to simplify the code

--- web/src/components/TradeDetail.tsx
+++ web/src/components/TradeDetail.tsx
@@ -1,12 +1,12 @@
-import * as Dialog from '@radix-ui/react-dialog';
+import * as Dialog from '@radix-ui/react-dialog'
 import '../styles/TradesDay.css'
-import clsx from 'clsx';
-import dayjs from 'dayjs';
-import { PencilSimple, Trash, X } from 'phosphor-react';
-import { EditTradeForm } from './EditTradeForm';
-import { DeleteTradeForm } from './DeleteTradeForm';
-import { useState } from 'react';
-import { FORMAT_STYLE } from '../lib/dayjs';
+import clsx from 'clsx'
+import dayjs from 'dayjs'
+import { PencilSimple, Trash } from 'phosphor-react'
+import { EditTradeForm } from './EditTradeForm'
+import { DeleteTradeForm } from './DeleteTradeForm'
+import { useState } from 'react'
+import { FORMAT_STYLE } from '../lib/dayjs'
 
 interface TradeProps {
   id: string,
@@ -17,15 +17,17 @@ interface TradeProps {
 }
 
 export function TradeDetail (props : TradeProps) { //Habit
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [showEditModal, setShowEditModal] = useState(false)
   
-  const handleTradeDeleted = () => {
-    setShowDeleteModal(!showDeleteModal); // Close the modal/container
-  };
-  const handleTradeEdited = () => {
-    setShowEditModal(!showEditModal); // Close the modal/container
-  };
+
+  function handleTradeEdited () {
+    setShowEditModal(false) // Close the modal/container
+  }
+  function handleTradeDeleted () {
+    setShowDeleteModal(false) // Close the modal/container
+  }
+
 
   return (
     <div className='mt-4 flex flex-col gap-3'>
@@ -54,6 +56,7 @@ export function TradeDetail (props : TradeProps) { //Habit
         <Dialog.Trigger 
           type='button' 
           className='mt-2 text-lg border border-green-700 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-green-200  transition-colors duration-150'
+          onClick={() => setShowEditModal(true)}
         >
           <PencilSimple size={20} className=' text-green-500' />
 
@@ -61,27 +64,16 @@ export function TradeDetail (props : TradeProps) { //Habit
         </Dialog.Trigger >
 
         {
-          showEditModal ||
+          showEditModal &&
           (
-            <Dialog.Portal>
-              <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0'/>
-              <Dialog.Content className='absolute p-10 bg bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-                <Dialog.Title className='text-3xl leading-tight font-extrabold'>
-                  Editar Trade
-                </Dialog.Title>
-                <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200'>
-                  <X size={24} aria-label='Fechar'/>
-                </Dialog.Close>
-                <EditTradeForm 
-                  id = {props.id}
-                  ticker = {props.ticker}
-                  result = {props.result}
-                  entryDate = {props.entry}
-                  exitDate = {props.exit}
-                  onTradeEdited = {handleTradeEdited}
-                />
-              </Dialog.Content>
-            </Dialog.Portal>
+            <EditTradeForm 
+              id = {props.id}
+              ticker = {props.ticker}
+              result = {props.result}
+              entryDate = {props.entry}
+              exitDate = {props.exit}
+              onShowTradeEdited = {handleTradeEdited}
+            />
           )
         }
       </Dialog.Root>
@@ -90,29 +82,18 @@ export function TradeDetail (props : TradeProps) { //Habit
         <Dialog.Trigger 
           type='button' 
           className=' font-semibold text-lg border border-red-700 rounded-lg px-6 py-4 flex items-center gap-3 hover:border-red-200  transition-colors duration-150'
+          onClick={() => setShowDeleteModal(true)}
         >
           <Trash size={20} className='text-red-500' />
-
           Excluir Trade
         </Dialog.Trigger >
 
-        { showDeleteModal || 
+        { showDeleteModal && 
           (
-            <Dialog.Portal>
-              <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0'/>
-              <Dialog.Content className='absolute p-10 bg bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-                <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200'>
-                  <X size={24} aria-label='Fechar'/>
-                </Dialog.Close>
-
-                <Dialog.Title className='text-3xl leading-tight font-extrabold'>
-                  Excluir Trade
-                </Dialog.Title>
-                <p>Are you sure you want to delete this trade?</p>
-                <DeleteTradeForm id={props.id} onTradeDeleted={handleTradeDeleted}/>
-              
-              </Dialog.Content>
-            </Dialog.Portal>
+            <DeleteTradeForm 
+              id={props.id} 
+              onShowTradeDeleted={handleTradeDeleted} 
+            />
           )
         }
       </Dialog.Root>
