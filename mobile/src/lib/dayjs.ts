eb59ab05796a596ee7177f24089d65321f4ff35e
Helio Andrade <hmajr.tec@gmail.com>
2024-08-23 08:45:03 -0300

    Create global data format

--- mobile/src/lib/dayjs.ts
+++ mobile/src/lib/dayjs.ts
@@ -1,4 +1,8 @@
 import dayjs from "dayjs";
-import 'dayjs/locale/pt-br';
+import 'dayjs/locale/pt-br'
+import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 
-dayjs.locale('pt-br');
+dayjs.extend(LocalizedFormat)
+
+export const FORMAT_STYLE = 'ddd, DD MMM YYYY HH:mm:ss'
+dayjs.locale('pt-br')
