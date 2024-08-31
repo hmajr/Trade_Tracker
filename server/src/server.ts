c03459054844556592fbd6424464c331c61e1ff4
Helio Andrade <hmajr.tec@gmail.com>
2024-08-31 09:18:03 -0300

    Continuos integration configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,8 +8,8 @@ app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-  host: '192.168.3.95',
-  port: 3333,
+  host: process.env.SERVER_ADDRESS,
+  port: Number(process.env.PORT) || 3333,
 }).then((data) => {
   console.log("HTTP Server running: \n", data)
 })
