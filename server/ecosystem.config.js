a89aceb15cdcedfd3cb2caf28671dba73e1aa794
Helio Andrade <hmajr.tec@gmail.com>
2024-08-31 15:35:25 -0300

    Update ecosystem config file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   apps : [{
-    name: "TradeTracker",
-    script: "./src/server.ts",
+    name: 'TradeTracker', // Replace with your app name
+    script: './src/server.ts', // Adjust if your main entry point is different
+    instances: 1, // Adjust based on your needs
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
     env: {
-      NODE_ENV: "development",
+      NODE_ENV: 'development'
     },
     env_production: {
-      NODE_ENV: "production",
+      NODE_ENV: 'production'
     }
-  }]
-}
+  }],
+};
