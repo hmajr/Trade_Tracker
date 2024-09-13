31f17ce29899549fe3e78211247f0f4d55c19ed7
Helio M. Andrade <hmajr@users.noreply.github.com>
2024-09-13 01:46:32 -0300

    Change server address

--- mobile/src/lib/axios.ts
+++ mobile/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL : "http://192.168.3.95:3333"
+  baseURL : "http://192.168.3.203:3333"
+  // baseURL : "http://localhost:3333"
 })
